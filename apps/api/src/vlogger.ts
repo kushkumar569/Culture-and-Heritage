3d9eb243e30f4b59c291543d3d534d3588d3f963
kushkumar569 <kushk569@gmail.com>
2025-10-10 01:12:54 +0530

    add upload backend

--- apps/api/src/vlogger.ts
+++ apps/api/src/vlogger.ts
@@ -1,3 +1,5 @@
+import e from "express";
+
 require("dotenv").config();
 const { Router: expressRouter } = require("express");
 const Vlogger = expressRouter();
@@ -5,9 +7,10 @@ const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
 
 Vlogger.get("/posts", async (req: any, res: any) => {
-    const userId = parseInt(req.query.userId);
+    const userId = parseInt(req.query.userId);  
+    
     try {
-        const posts = await client.Post.find({
+        const posts = await client.Post.findMany({
             where: { userId: userId },
         })
         res.status(200).json(posts);
@@ -18,9 +21,26 @@ Vlogger.get("/posts", async (req: any, res: any) => {
 })
 
 Vlogger.post("/upload", async (req: any, res: any) => {
-    const { title, content, mediaUrl, type, userId } = req.body;
+    const { title, content, mediaUrl, type, userId, place } = req.body;
     console.log(title, content, mediaUrl, type, userId);
-    
+    try{
+        const data = await client.Post.create({
+            data: {
+                userId: userId,
+                title: title,
+                content: content,
+                mediaUrl: mediaUrl,
+                type: type,
+                place: place
+            }
+        })
+        if(data){
+           return res.status(200).json("Post Uploaded");
+        }
+        return res.status(404).json("post uploading failed")
+    } catch(error){
+        return res.json("internal server error",error)
+    }
 })
 
 module.exports = Vlogger
